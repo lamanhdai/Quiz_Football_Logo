43ce78d40d6429eb1e2b786b814ab5bb5e30e909
Tu Nguyen <nvminhtu@gmail.com>
2016-05-14 19:36:35 +0700

    refs #update first feature - withou counter (quiz)

--- js/app.js
+++ js/app.js
@@ -1,38 +1,85 @@
 var app = angular.module('quizApp', ['quiz.controllers','quiz.services']);
 
-
 // Custom directive
-app.directive('quiz', function(quizFactory) {
+app.directive('quiz', function(quizFactory,$compile,$timeout) {
 	return {
-		restrict: 'AE',
+		restrict: 'AEC',
 		scope: {
 			inputValue: '='
 		},
 		templateUrl: 'template.html',
-		 // bindToController: true,
-	  //    controllerAs: 'ctrl',
-	  //    controller: function(){
-	  //        this.getClass(){
-	  //          return (ctrl.inputValue === 'something' ? "myButton1" : "myButton2");
-	  //        }
-	  //    },
 		link: function(scope, elem, attrs) {
-			scope.getQuestions = quizFactory.getQuestions();
-			scope.ClassName = "test";
+			var timer;
 
+			scope.getQuestions = quizFactory.getQuestions();
+			
 			scope.start = function() {
 				scope.id = 0;
 				scope.quizOver = false;
 				scope.inProgress = true;
 				scope.checkNull = false;
 				scope.getQuestion();
+
+				//Start counter 
+				scope.second = 0;
+				scope.minute = formatDigi(0);
+				scope.hour = formatDigi(0);
+
+				var updateCounter = function() {
+					if(scope.second == 60) {
+						scope.second = 0;
+						scope.minute++;
+						scope.minute = formatDigi(scope.minute);
+					}
+					if(scope.minute == 60 ){
+						scope.minute = formatDigi(0);
+						scope.hour++;
+						scope.hour = formatDigi(scope.hour);	
+					}
+					scope.second++;
+					scope.second = formatDigi(scope.second);
+		            timer = $timeout(updateCounter, 1000);
+		        };
+		        updateCounter();
+
 			};
+			
+			scope.calcTimer = function() {
+				var 
+					hours = scope.hour * 3600;
+					minutes = scope.minute * 60;
+					seconds = scope.second;
+					total = 0;
+				// if(hours > 0)
+				// 	total += hours; 
+				// if(minutes > 0)
+				// 	total += minutes;
+				// total += seconds;
+
+				total = hours + minutes + seconds;
+				return formatDigi(total);
+			}
+
+			//Format
+			function formatDigi(d) {
+				return (d < 10) ? '0' + d.toString() : d.toString();
+			}
 
 			scope.reset = function() {
 				scope.quizOver = false;
 				scope.inProgress = false;
 				scope.score = 0;
+
+				scope.second = 0;
+				scope.minute = 0;
+				scope.hour = 0;
+
 			}
+
+			//Stop counter
+			scope.stopCounter = function() {
+    		    $timeout.cancel(timer);
+    		};
 
 			scope.getQuestion = function() {
 				var q = quizFactory.getQuestion(scope.id);
@@ -72,6 +119,9 @@ app.directive('quiz', function(quizFactory) {
 			scope.nextQuestion = function() {
 				scope.id++;
 				scope.getQuestion();
+				if(scope.quizOver == true ) {
+					$timeout.cancel(timer);
+				}
 			}
 
 			scope.reset();
@@ -80,7 +130,7 @@ app.directive('quiz', function(quizFactory) {
 });
 
 // Filter: Replace item for unique name
-app.filter('customKeyFormat',function(){
+app.filter('customKeyFormat',function() {
 	return function(input){
 		var nameStr = input.replace(" ","");
 		return nameStr;
